4338a8fa7d397c606cfa94851f934af38b12294f
조원희 <wonheejo@gmail.com>
2026-03-14 15:34:25 +0900

    Use Fira Code as the sole font for all text

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const haffer = localFont({
-  src: [
-    {
-      path: "../public/fonts/Haffer-Light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Haffer-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Haffer-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-  ],
-  variable: "--font-haffer",
-});
-
 const firaCode = localFont({
   src: [
     {
@@ -46,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${haffer.variable} ${firaCode.variable} antialiased`}>
+      <body className={`${firaCode.variable} antialiased`}>
         <div className="min-h-screen bg-background text-foreground">
           {children}
         </div>
